ae31cb27d7990c5dc12fb665bf39eb0ae376b00b
spChalk <sdi1800209@di.uoa.gr>
2021-08-24 16:49:01 +0300

    added email validation

--- src/main/java/com/example/socialnetworkingapp/registration/EmailValidatorService.java
+++ src/main/java/com/example/socialnetworkingapp/registration/EmailValidatorService.java
@@ -3,12 +3,18 @@ package com.example.socialnetworkingapp.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidatorService implements Predicate<String> {
+    /* Source: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression */
     @Override
     public boolean test(String s) {
-        //TODO : regex to validate email, for example chris@something.com is valid but example.com is not a valid mail
-        return true;
+
+        String pattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern r = Pattern.compile(pattern);
+        // Create matcher object.
+        return r.matcher(s).find();
     }
 }
